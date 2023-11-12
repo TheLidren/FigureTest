4747a2b44b87c07852f5bb697b5636a2c18b6d4d
Vladislav Misevich <vlad.misevich.2003@mail.ru>
2023-11-12 15:29:56 +0300

    Добавьте файлы проекта.

---
+++ SquareLib/SquareLib/Figure.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureLib
+{
+    public class Figure
+    {
+        protected double area;
+        protected string? s;
+        public virtual void Area()
+        {
+            Console.WriteLine($"Area");
+        }
+    }
+    public class Triangle : Figure
+    {
+        private int a, b, c;
+        public Triangle(int a, int b, int c) 
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }  
+        public override void Area()
+        {
+            if (Math.Pow(a, 2) == Math.Pow(b + c, 2) || Math.Pow(b, 2) == Math.Pow(a + c, 2) || Math.Pow(c, 2) == Math.Pow(b + a, 2))
+                Console.WriteLine("Right triangle");
+            int p = (a + b + c) / 2;
+            area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            Console.WriteLine($"Area of triangle = {area:F3}");
+        }
+    }
+    public class Circle : Figure
+    {
+        public override void Area()
+        {
+            Console.Write("Enter r = ");
+            s = Console.ReadLine();
+            if (!int.TryParse(s, out int r))
+                throw new Exception("U enter incorrectly number");
+            area = Math.PI * r;
+            Console.WriteLine($"Area of circle = {area:F2}");
+        }
+    }
+    public class Square : Figure
+    {
+        public override void Area()
+        {
+            Console.Write("Enter a = ");
+            s = Console.ReadLine();
+            if (!int.TryParse(s, out int a))
+                throw new Exception("U enter incorrectly number");
+            area = Math.Pow(a, 2);
+            Console.WriteLine($"Area of circle = {area}");
+        }
+    }
+
+
+}
---
+++ Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using FigureLib;
+
+Console.WriteLine("Choise a figure: 'circle', 'triangle', 'square', 'rectangle'");
+var text = Console.ReadLine();
+Figure figure;
+switch (text.ToLower())
+{
+    case "circle":
+        {
+			figure = new Circle();
+            figure.Area();
+            break; 
+        }
+    case "triangle":
+        {
+            int a, b, c;
+            try
+            {
+                Console.Write("Enter a = ");
+                a = int.Parse(Console.ReadLine());
+                Console.Write("Enter b = ");
+                b = int.Parse(Console.ReadLine());
+                Console.Write("Enter c = ");
+                c = int.Parse(Console.ReadLine());
+                if (a <= 0 || b <= 0 || c <= 0)
+                    throw new Exception("Number <= 0");
+                figure = new Triangle(a, b, c);
+                figure.Area();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+			break; 
+        }
+    case "square":
+        {
+            figure = new Square();
+            figure.Area();
+            break;
+        }
+	default:
+        {
+            Console.WriteLine("Type of figure doesn't find");
+            break;
+        }
+}
+Console.ReadLine();
